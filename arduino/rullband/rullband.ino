7c8c765df07ec0bf5c8854364235a80a6e05ad7a
Fredrik Löfgren <frelo223@student.liu.se>
2017-12-17 18:35:37 +0100

    Updated arduino code to accept serial data from computer

--- arduino/rullband/rullband.ino
+++ arduino/rullband/rullband.ino
@@ -4,88 +4,179 @@
   DI  >>> D11
 */
 #include <SPI.h>
-byte address = 0x11; // Select the first potentiometer, and write data to it
+#include <EEPROM.h>
+
+struct handle {
+  byte t0;
+  byte t1;
+  byte t2;
+  char d0;
+  unsigned long getTime() {
+    return ((unsigned long)t2 << 16) | ((unsigned long)t1 << 8) | (t0);
+  }
+  int getHeight() {
+    return d0;
+  }
+};
+
+
+
+
+// Setup pins
 int CS= 10;          // Chip select
 int i=0;
-
 int forward = A0;
 int backward = A3;
 
+// Adress to the digital potentiometer
+byte address = 0x11; 
+
+// Variables for receiving new programs
+unsigned long lastRecieved;
+const unsigned long TIME_THRESHOLD = 5000;
+byte value = 0;
+int index = 0;
+bool uploadInProgress = false;
+
+
+// Variables for execution of program
+float current_time;
+byte handle_index = 0;
+handle current_handle;
+handle next_handle;
+bool done = false;
+byte lengthOfProgram = 0;
+
+
+void(* resetFunc) (void) = 0;//declare reset function at address 0
+
+
 void setup()
 {
+  Serial.begin(9600);
   pinMode(CS, OUTPUT);
   pinMode(forward, OUTPUT);
   pinMode(backward, OUTPUT);
   SPI.begin();
 
+  EEPROM.get(0, lengthOfProgram);
+  EEPROM.get(1, current_handle);
+  EEPROM.get(1 + sizeof(handle), next_handle);
+  
   // Stop the band at startup
-  stopping();
-  // Wait 10s before start
-  delay(10000); 
+  noMotion();
+  Serial.println("Start execution of program"); 
 }
 
+void loop()
+{
+
+  // If we have a new program sent to us. 
+  if (Serial.available() > 0) {
+    uploadInProgress = true;
+    // If the program is a new one (instead of continuation of a ongoing program download)
+    if(millis() - lastRecieved > TIME_THRESHOLD){
+      Serial.println("New program detected");
+      index = 0;
+    }
+    if(index >= EEPROM.length()){
+      Serial.println("Reached end of eeprom memory");
+      index = 0;
+    }
+    // read the incoming byte:
+    value = Serial.read();
+  
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(value, DEC);
+
+    // Save value to EEPROM memory
+    EEPROM[index] = value;
+
+    // Increase index to next iteration 
+    index++;
+    lastRecieved = millis();
+  }
+
+
+  // New program upload done
+  if(uploadInProgress && millis() - lastRecieved > TIME_THRESHOLD){
+    Serial.println("Upload of new program done");
+    uploadInProgress = false;
+    resetFunc();  //call reset
+  }
+
+  if(uploadInProgress){
+    return;
+  }
+  
+  if(done){
+    Serial.println("DONE!");
+    delay(4000);
+    return;
+  }
+  current_time = ((float)millis()/1000.0);
+
+  while (current_time > next_handle.getTime()) {
+    Serial.println("Update handle to next");
+    handle_index += sizeof(handle);
+    if(handle_index > (lengthOfProgram-2) * sizeof(handle)){
+      done = true;
+      return;
+    }
+    current_handle = next_handle;
+    EEPROM.get(1+handle_index + sizeof(handle), next_handle);
+    return;
+  }
+  //Serial.print("Current handle: ");
+  //printHandle(current_handle);
+  char speed = interpolate(current_handle, next_handle, current_time);
+  setSpeed(speed);
+  delay(400);
+}
+
+
+
+char interpolate(struct handle p0, struct handle p1, float t) {
+  signed long x0 = p0.getTime();
+  signed long x1 = p1.getTime();
+  char y0 = p0.getHeight();
+  char y1 = p1.getHeight();
+  return y0 + (t - x0) * (y1 - y0) / (x1 - x0);
+}
+
+
 // Set control signals to backward direction
-void goBackward(){
+void backwardMotion(){
     digitalWrite(forward, LOW);
     digitalWrite(backward,HIGH);
 }
 
 // Set control signals to forward direction
-void goForward(){
+void forwardMotion(){
     digitalWrite(backward, LOW);
     digitalWrite(forward,HIGH);
 }
 
 // Set control signals to no motion
-void stopping(){
+void noMotion(){
     digitalWrite(backward, LOW);
     digitalWrite(forward,  LOW);
 }
 
-void loop()
+// Set speed of the band (value range -128 to 127)
+int setSpeed(char value)
 {
-    goForward();
+  if(value < 0)
+    backwardMotion();
+  else if (value > 0)
+    forwardMotion();
+  else if(value == 0)
+    noMotion();
 
-    // Ramp up speed forward direction
-    for (i = 0; i <= 255; i++)
-    {
-      digitalPotWrite(i);
-      delay(50);
-    }
-    delay(1000);
-    // Ramp down speed forward direction
-    for (i = 255; i >= 0; i--)
-    {
-      digitalPotWrite(i);
-      delay(50);
-    }
-    delay(3000);
-
-
-    goBackward();
-    
-    // Ramp up speed backward direction
-    for (i = 0; i <= 255; i++)
-    {
-      digitalPotWrite(i);
-      delay(50);
-    }
-    delay(1000);
-    // Ramp down speed backward direction
-    for (i = 255; i >= 0; i--)
-    {
-      digitalPotWrite(i);
-      delay(50);
-    }
-    delay(3000);
-}
-
-
-// Write data to the digital potentiometer
-int digitalPotWrite(int value)
-{
+  
   digitalWrite(CS, LOW);
   SPI.transfer(address);
-  SPI.transfer(value);
+  SPI.transfer(abs(value));
   digitalWrite(CS, HIGH);
 }
